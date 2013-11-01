cce1c06c9f79caf47060b6dda44e5b1342adf4bb
deviator <code.viator@gmail.com>
2013-11-02 00:13:59 +0400

    add some types

--- import/dvf/types.d
+++ import/dvf/types.d
@@ -12,3 +12,30 @@ struct DVF_FileMeta
     ushort layers;
     ushort[2] res;
 }
+
+struct DVF_LayerHeader
+{
+    ushort id;
+    char[256] name;
+    ubyte comps;
+    ubyte type;
+    short[2] pos;
+    ushort[2] res;
+    ushort mask_id;
+}
+
+enum
+{
+    DVF_NONE = 0x0000,
+    DVF_TYPE_BYTE,
+    DVF_TYPE_UBYTE,
+    DVF_TYPE_SHORT,
+    DVF_TYPE_USHORT,
+    DVF_TYPE_INT,
+    DVF_TYPE_UINT,
+    DVF_TYPE_LONG,
+    DVF_TYPE_ULONG,
+
+    DVF_TYPE_FLOAT,
+    DVF_TYPE_DOUBLE
+}
